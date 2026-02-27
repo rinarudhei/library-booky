2cfbc2952bd1cc4a46ab78b655c72f637dd2a395
rinarudhei <rinaldiadrian5@gmail.com>
2026-02-27 08:53:52 +0700

    feat: add userSlice in auth feature

--- src/services/stores/allReducers.ts
+++ src/services/stores/allReducers.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import authReducers from '@/app/auth/authSlice';
+import userReducers from '@/app/auth/userSlice';
 
 export const allreducers = combineReducers({
   auth: authReducers,
+  user: userReducers,
 });
